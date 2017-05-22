f6b75001431af5cec6c0f8ae497e56492af8d179
Martin Delfs <Martind_64@hotmail.com>
2017-05-22 12:28:49 +0200

    Added google maps plugin and it works on mapsPage

--- src/pages/maps-page/maps-page.ts
+++ src/pages/maps-page/maps-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -7,6 +7,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
+declare var google;
 @IonicPage()
 @Component({
   selector: 'page-maps-page',
@@ -14,11 +15,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapsPage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapsPage');
+  	this.loadMap();
+  }
+
+  loadMap(){
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
 }
