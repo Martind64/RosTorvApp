a0d6e37808407d385eff25955d0b815027da4883
MarkPedersen <MarkPedersen1991@hotmail.com>
2017-05-22 11:58:04 +0200

    Added frontpage and event page list -> pure static no connection to api yet.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EventsPage } from '../events-page/events-page';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+public eventsPage=EventsPage;
+
+
+  constructor(public navCtrl: NavController) { }
+  	eventPage(){
+  		this.navCtrl.push(EventsPage);
+  	}
 
   }
 
-}
+
