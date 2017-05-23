0c65ed3a76026bbe6c61a3242a4ce2dc751aea21
Martin Delfs <Martind_64@hotmail.com>
2017-05-23 13:01:57 +0200

    WORKS?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,13 @@ import { CategoryPage } from '../pages/category-page/category-page';
 import { ShopsPage } from '../pages/shops-page/shops-page';
 import { HomePage } from '../pages/home/home';
 import { MapsPage } from '../pages/maps-page/maps-page';
+import { EventsPage } from '../pages/events-page/events-page';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = MapsPage;
+  rootPage:any = EventsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
