4773b2fca8e2e35438d35b263e1eccdba0bf8c50
MarkPedersen <MarkPedersen1991@hotmail.com>
2017-05-29 13:27:18 +0200

    Fixed home - centering things. fixed overskrift til shops-page

--- src/pages/shops-page/shops-page.ts
+++ src/pages/shops-page/shops-page.ts
@@ -17,6 +17,7 @@ import { CategoryPage } from '../category-page/category-page';
 export class ShopsPage {
 
   public stores = [];
+  public category:string;
 
   
   constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
@@ -29,29 +30,37 @@ export class ShopsPage {
           switch (store) {
             case "elektronik":
               this.stores = ['Fona', 'GameStop', 'MobileTec', 'Telia']
+              this.category = "Elektronik";
               break;
              case "Mad":
              this.stores = ['Burger King', 'Sunset', 'Joe and the Juice', 'Flammen', 'Baresso', 'Café Vivaldi', 'Nachos', 'Ristorante La Rustica', 'The Spot Cafe', 'Zhiki Sushi']
+             this.category = "Mad";
              break;
              case "Sport":
              this.stores = ['Fitness DK', 'SportsMaster', 'Sport 24']
+             this.category = "sport";
              break;
              case "Dagligdagsvare":
              this.stores = ['Føtex', 'Frellsen', 'Tiger', 'Roskilde Vin']
+             this.category = "DaligdagsVare";
              break;
              case "BoligLivstil":
              this.stores = ['Bolia', 'Imerco', 'Søsterne Grene', 'Flying Tiger Copenhagen', 'Le Creuset']
+             this.category = "Bolig Livstil";
              break;
              case "Tøj":
              this.stores = ['H&M', 'Kings And Queens', 'Tøjexperten', 'Luciano Grosso', 'Mr. Møllebach', 'No8', 'Sams', 'Samsøe Samsøe', 'Suitmeup', 'Superdry', 'Trendstar']
+             this.category ="Herre Tøj";
              break;
              case "Damemode":
              this.stores = ['Bonde Boutique', 'By Mo', 'Companys', 'Copenhagen Luxe', 'Deres', 'Esprit', 'H&M', 'Hunkemôller', 'Kings & Queens', 'Message', 'Only', 'Pieces', 'Samsøe & Samsøe', 'Schack & Thorsten', 'Solo', 'Superdry', 'Trendstar', 'Triumph', 'Vila']
+             this.category = "Dame Mode";
              break;
              case "SkønhedSundhed":
              this.stores = ['Apoteket RO’s Torv', 'Beauty by Boozt.com', 'Matas', 'Nice Thai Massage', 'Normal', 'Queens Nails', 'Salon Unique']
+             this.category = "Skønhed & Sunhed";
              break;
-            default:
+             default:
               console.log("Shit happend!")
               break;
           }
